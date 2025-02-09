c657ca06c6ff6c3fc3f461c942496781269d711e
Aclamendo <davidjones@ycre.net>
2025-02-09 14:58:45 -0700

    minimum prototype

--- Scripts/game.gd
+++ Scripts/game.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+var rolling_rock = load("res://rolling_rock.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -18,3 +19,18 @@ func _zoom_out():
 func _zoom_in():
 	$Player/PlayerCamera.make_current()
 	$CameraSwapTimer.start()
+
+# Roll to bottom
+func _on_summit_zone_body_entered(body: Node2D) -> void:
+	$Player.handle_summit()
+	$CameraSwapTimer.stop()
+	var roller = rolling_rock.instantiate()
+	roller.position = $Player.position
+	get_parent().add_child(roller)
+	$GlobalCamera.make_current()
+
+
+func _on_bottom_zone_body_entered(body: Node2D) -> void:
+	print("Returned")
+	$Player.reset_summit()
+	body.queue_free()
---
+++ Scripts/player.gd
@@ -0,0 +1,80 @@
+extends CharacterBody2D
+
+const moveX = 2
+const moveY = -1.55
+@export var moveRate: float = 10
+
+var player_started = false
+
+var summited = false
+var start_pos
+
+const GROUND = 433
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+func _ready():
+	start_pos = position
+
+func _process(delta: float):
+	if summited:
+		return
+	if Input.is_action_just_pressed("right") and position.y < GROUND:
+		if not player_started:
+			$UI.visible = false
+			player_started = true
+		_pushRock()
+
+func _physics_process(delta: float) -> void:
+	if summited:
+		move_and_slide()
+		return
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	 #Get the input direction and handle the movement/deceleration.
+	 #As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("left", "right")
+	if direction and position.y > GROUND:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
+
+func _pushRock():
+	# Move the rock
+	position.x += moveX * moveRate
+	position.y += moveY * moveRate
+	
+	# TODO animate sprite and play sound
+	
+func handle_summit():
+	print("Summited")
+	summited = true;
+	$AnimatedSprite2D.visible = false;
+	$BoundingBox.visible = false;
+	$Label.visible = false;
+	
+	# TODO Play return animation
+	
+func reset_summit():
+	summited = false;
+	$AnimatedSprite2D.visible = true;
+	$BoundingBox.visible = true;
+	$Label.visible = true;
+	$PlayerCamera.make_current()
+	
+	position = start_pos
+	$UI.visible = true
+	player_started = true
+	
+	# TODO Reset sprites to start
+	
+	
